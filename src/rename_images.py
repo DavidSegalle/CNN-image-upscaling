c7e2ae6d2827f25140f21d58880a1cfe182945ae
David Segalle <davidssegalle@alunos.utfpr.edu.br>
2025-02-18 17:06:44 -0300

    Working training (I think)

--- src/rename_images.py
+++ src/rename_images.py
@@ -1,48 +1,5 @@
-import kagglehub
-from create_model import load_res_images
-import matplotlib.pyplot as plt
 import os
-from PIL import Image
 import cv2
-
-# Download latest version
-path = kagglehub.dataset_download("awsaf49/coco-2017-dataset")
-
-path += "/coco2017"
-
-sep = 'src'
-stripped = __file__.split(sep, 1)[0]
-
-print("Copying dataset to desired location")
-
-os.system("cp -r " + path + " " + stripped)
-os.system("rm -rf " + stripped + "/coco2017/annotations")
-
-def delete_non_640x480_images(folder_path):
-    for filename in os.listdir(folder_path):
-        file_path = os.path.join(folder_path, filename)
-        
-        try:
-            with Image.open(file_path) as img:
-                if img.size != (640, 480):
-                    os.remove(file_path)
-                    #print(f"Deleted: {filename} (Size: {img.size})")
-                #else:
-                #    print(f"Kept: {filename} (Size: {img.size})")
-        except Exception as e:
-            print(f"Skipping: {filename} (Error: {e})")
-
-print("Deleting wrong resolution test images")
-folder = "../coco2017/test2017"
-delete_non_640x480_images(folder)
-
-print("Deleting wrong resolution train images")
-folder = "../coco2017/train2017"
-delete_non_640x480_images(folder)
-
-print("Deleting wrong resolution val images")
-folder = "../coco2017/val2017"
-delete_non_640x480_images(folder)
 
 def rename_images(folder_path, prefix="image"):
     image_extensions = (".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff")
@@ -75,7 +32,6 @@ print("Renaming test images")
 folder_path = "../coco2017/test2017"
 rename_images(folder_path)
 
-# Creating a copy of the dataset in low resolution (in order to allow only loading parts of it)
 def downscale_images(folder_path, save_path):
     image_extensions = (".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff")
     images = [f for f in os.listdir(folder_path) if f.lower().endswith(image_extensions)]
--- src/train_model.py
+++ src/train_model.py
@@ -5,6 +5,7 @@ import os
 from keras.api.utils import Sequence
 from keras.api import models, layers, optimizers, callbacks
 from keras.api.utils import image_dataset_from_directory
+from keras.api.layers import Rescaling
 import numpy as np
 import matplotlib.pyplot as plt
 from keras.api.models import Model
@@ -19,16 +20,55 @@ from create_model import build_model, load_res_images
 sep = 'src'
 stripped = __file__.split(sep, 1)[0]
 print(stripped)
-path = stripped + "/coco2017"
+high_path = stripped + "coco2017"
+low_path = stripped + "low_coco"
 
-high_res_train_path = path + "/train2017"
+'''high_res_train_path = path + "/train2017"
 
 high_res_test_path = path + "/test2017"
 
 print("\n\nloading images\n\n")
 train_low_images, train_high_images = load_res_images(high_res_train_path, 320, 240, 800)
 
-test_low_images, test_high_images = load_res_images(high_res_test_path, 320, 240, 200)
+test_low_images, test_high_images = load_res_images(high_res_test_path, 320, 240, 200)'''
+
+class ImagePairGenerator(Sequence):
+    def __init__(self, low_res_path, high_res_path, batch_size, input_size, output_size):
+        self.low_res_files = sorted(os.listdir(low_res_path))
+        self.high_res_files = sorted(os.listdir(high_res_path))
+        self.low_res_path = low_res_path
+        self.high_res_path = high_res_path
+        self.batch_size = batch_size
+        self.input_size = input_size   # Low-resolution size
+        self.output_size = output_size # High-resolution size
+
+    def __len__(self):
+        return int(np.floor(len(self.low_res_files) / self.batch_size))
+
+    def __getitem__(self, index):
+        batch_low = self.low_res_files[index * self.batch_size:(index + 1) * self.batch_size]
+        batch_high = self.high_res_files[index * self.batch_size:(index + 1) * self.batch_size]
+
+        # Load and resize images
+        low_res_imgs = np.array([
+            cv2.resize(cv2.imread(os.path.join(self.low_res_path, f)), self.input_size)
+            for f in batch_low
+        ]) / 255.0
+        
+        high_res_imgs = np.array([
+            cv2.resize(cv2.imread(os.path.join(self.high_res_path, f)), self.output_size)
+            for f in batch_high
+        ]) / 255.0
+
+        return low_res_imgs, high_res_imgs
+
+input_size = (320, 240)   # Example for low-resolution images
+output_size = (640, 480)
+print(low_path)
+print(high_path + "\n\n\n")
+print(os.listdir(high_path))
+train_generator = ImagePairGenerator(low_path + "/train2017", high_path + "/train2017", batch_size=16, input_size=input_size, output_size=output_size)
+test_generator = ImagePairGenerator(low_path + "/test2017", high_path + "/test2017", batch_size=16, input_size=input_size, output_size=output_size)
 
 
 # Simple test to check if images are being loaded properly and check if downscale isn't too sharp
@@ -61,21 +101,10 @@ cp_callback = callbacks.ModelCheckpoint(filepath=checkpoint_path,
                                                  save_weights_only=True,
                                                  verbose=1)
 
-# Shennanigans to keep the dataset outside of vram
-with tf.device('/CPU:0'):
-    train_high_images = tf.constant(train_high_images)
-    train_low_images = tf.constant(train_low_images)
-    test_low_images = tf.constant(test_low_images)
-    test_high_images = tf.constant(test_high_images)
-
-
-
 history = model.fit(
-    train_low_images,
-    train_high_images,
+    train_generator,
     epochs = 10,
-    batch_size = 16,
-    validation_data=(test_low_images, test_high_images),
+    validation_data=test_generator,
     verbose = 1,
     callbacks=[cp_callback]
 )
