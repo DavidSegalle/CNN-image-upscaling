a960ce3ebbbe2388e041395d8cfcd2924787bd5c
David Segalle <davidssegalle@alunos.utfpr.edu.br>
2025-02-18 14:57:42 -0300

    Download dataset has all features for not loading dataset to ram

--- src/download_dataset.py
+++ src/download_dataset.py
@@ -3,19 +3,17 @@ from create_model import load_res_images
 import matplotlib.pyplot as plt
 import os
 from PIL import Image
+import cv2
 
 # Download latest version
 path = kagglehub.dataset_download("awsaf49/coco-2017-dataset")
 
 path += "/coco2017"
 
-print("Path to dataset files:", path)
-
-print(__file__)
-
 sep = 'src'
 stripped = __file__.split(sep, 1)[0]
-print(stripped)
+
+print("Copying dataset to desired location")
 
 os.system("cp -r " + path + " " + stripped)
 os.system("rm -rf " + stripped + "/coco2017/annotations")
@@ -34,12 +32,16 @@ def delete_non_640x480_images(folder_path):
         except Exception as e:
             print(f"Skipping: {filename} (Error: {e})")
 
-# Example usage
-folder = "../coco2017/test2017"  # Change this to your image folder path
+print("Deleting wrong resolution test images")
+folder = "../coco2017/test2017"
 delete_non_640x480_images(folder)
-folder = "../coco2017/train2017"  # Change this to your image folder path
+
+print("Deleting wrong resolution train images")
+folder = "../coco2017/train2017"
 delete_non_640x480_images(folder)
-folder = "../coco2017/val2017"  # Change this to your image folder path
+
+print("Deleting wrong resolution val images")
+folder = "../coco2017/val2017"
 delete_non_640x480_images(folder)
 
 def rename_images(folder_path, prefix="image"):
@@ -55,16 +57,46 @@ def rename_images(folder_path, prefix="image"):
         new_path = os.path.join(folder_path, new_name)
         
         os.rename(old_path, new_path)
-        print(f"Renamed: {filename} -> {new_name}")
+        #print(f"Renamed: {filename} -> {new_name}")
     
-    print("Renaming completed!")
+    #print("Renaming completed!")
 
 # Renaming images back into ascending order without skips (important for indexing reasons)
+print("Renaming train images")
 folder_path = "../coco2017/train2017"
 rename_images(folder_path)
 
+print("Renaming val images")
 folder_path = "../coco2017/val2017"
 rename_images(folder_path)
 
+print("Renaming test images")
 folder_path = "../coco2017/test2017"
 rename_images(folder_path)
+
+# Creating a copy of the dataset in low resolution (in order to allow only loading parts of it)
+def downscale_images(folder_path, save_path):
+    image_extensions = (".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff")
+    images = [f for f in os.listdir(folder_path) if f.lower().endswith(image_extensions)]
+
+    images.sort()
+    
+    if not os.path.isdir("../low_coco"):
+        os.mkdir("../low_coco")
+
+    if not os.path.isdir(save_path):
+        os.mkdir(save_path)
+
+    for index, filename in enumerate(images, start=1):
+        img = cv2.imread(folder_path + "/" + filename)
+        resized_image = cv2.resize(img, (320, 240), interpolation=cv2.INTER_NEAREST)
+        cv2.imwrite(save_path + "/" + filename, resized_image)
+
+print("Creating downscaled test images")
+downscale_images("../coco2017/test2017", "../low_coco/test2017")
+
+print("Creating downscaled train images")
+downscale_images("../coco2017/train2017", "../low_coco/train2017")
+
+print("Creating downscaled val images")
+downscale_images("../coco2017/val2017", "../low_coco/val2017")
